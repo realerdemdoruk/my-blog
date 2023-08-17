6495316a7437fa1fca83cec9c680f8baba5048d7
Erdem <erdemdoruk3@gmail.com>
2023-08-18 02:33:54 +0300

    Dark mode localStorage'ye geçildi.

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { allPosts } from "@/.contentlayer/generated";
 import Mdx from "@/app/components/Mdx";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { notFound } from "next/navigation";
 import { Balancer } from "react-wrap-balancer";
+import { TypeAnimation } from "react-type-animation";
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -17,10 +19,32 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
     return notFound();
   }
 
+  const TypingAnimation = ({ title = "" }) => {
+    return (
+      <TypeAnimation
+        sequence={[title, 2000, " ", 100]}
+        wrapper="span"
+        preRenderFirstString={true}
+        speed={50}
+        style={{ fontSize: "1em", display: "inline-block" }}
+        repeat={Infinity}
+      />
+    );
+  };
+
   return (
     <article className="z-10  mx-auto max-w-3xl flex flex-col w-full px-4 py-10">
-      <h1 className="text-2xl font-bold mb-2 dark:text-slate-300">
-        <Balancer>{post.title}</Balancer>
+      <h1
+        className="text-2xl font-bold mb-2 dark:text-slate-300
+      duration-1000 ease-in-out transition-all"
+      >
+<<<<<<< HEAD
+        <Balancer>
+          <TypingAnimation title={post.title} />
+        </Balancer>
+=======
+        <TypingAnimation title={post.title} />
+>>>>>>> 69accf66d5bada1f08eaa5ac4227dc622a140551
       </h1>
 
       <Mdx code={post.body.code} />
--- app/components/Mdx.tsx
+++ app/components/Mdx.tsx
@@ -1,6 +1,5 @@
 import { useMDXComponent } from "next-contentlayer/hooks";
 import RoundedImage from "./RoundedImage";
-import CustomLink from "./CustomLink";
 import { CopyButton } from "./CopyButton";
 
 // @ts-ignore
@@ -16,13 +15,19 @@ export const Pre = ({ children, raw, ...props }) => {
 
 const components = {
   Image: RoundedImage,
-  a: CustomLink,
   pre: Pre,
   h2: (props: any) => (
-    <h2 className="text-2xl font-bold  dark:text-slate-300" {...props} />
+    <h2
+      className="text-2xl font-bold duration-1000 ease-in-out transition-all dark:text-slate-300"
+      {...props}
+    />
   ),
   h3: (props: any) => (
-    <h2 className="text-2xl font-bold  dark:text-slate-400" {...props} />
+    <h2
+      className="text-2xl font-bold
+      duration-1000 ease-in-out transition-all dark:text-slate-400"
+      {...props}
+    />
   ),
 };
 
@@ -33,7 +38,10 @@ type MdxProps = {
 export default function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
   return (
-    <div className="prose prose-pink dark:text-white ">
+    <div
+      className="prose prose-pink dark:text-white
+    duration-1000 ease-in-out transition-all"
+    >
       {/* @ts-ignore */}
       <Component components={components} />
     </div>
