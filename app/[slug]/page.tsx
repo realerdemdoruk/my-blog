a1ba692b274b1a8a87081a986f16dfa54b6f297f
Erdem <erdemdoruk3@gmail.com>
2023-08-18 03:03:43 +0300

    Tamamlandı

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,9 +1,10 @@
+"use client";
 import { allPosts } from "@/.contentlayer/generated";
 import Mdx from "@/app/components/Mdx";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { notFound } from "next/navigation";
 import { Balancer } from "react-wrap-balancer";
-
+import { TypeAnimation } from "react-type-animation";
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
     slug: post.slug,
@@ -16,14 +17,27 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
   if (!post) {
     return notFound();
   }
-
+  const TypingAnimation = ({ title = "" }) => {
+    return (
+      <TypeAnimation
+        sequence={[title, 2000, " ", 100]}
+        wrapper="span"
+        preRenderFirstString={true}
+        speed={50}
+        style={{ fontSize: "1em", display: "inline-block" }}
+        repeat={Infinity}
+      />
+    );
+  };
   return (
     <article className="z-10  mx-auto max-w-3xl flex flex-col w-full px-4 py-10">
       <h1
         className="text-2xl font-bold mb-2 dark:text-slate-300
       duration-1000 ease-in-out transition-all"
       >
-        <Balancer>{post.title}</Balancer>
+        <Balancer>
+          <TypingAnimation title={post.title} />
+        </Balancer>
       </h1>
 
       <Mdx code={post.body.code} />
